13d0e1f074237fc0e1bef1e0421474950ed2dc53
Dinara <dinozavrik.dina95@gmail.com>
2019-09-08 19:30:45 +0200

    test_python

---
+++ test.py
@@ -0,0 +1,34 @@
+# n=5
+# x=4
+# y=0
+# spaces=""
+# stars="*"
+# for i in range(n):
+#     for j in range (x, x-1):
+#             for k in range (y,2*y+1):
+#                     print("/n"+"stars"+"spaces")
+        
+# n=5
+# stars=" "
+# for i in range(n):
+#     for nb_spaces in range(n-i-1):
+#         stars+=" "
+#         for nb_stars in range (2*i-1):
+#             stars+="*"
+#             stars=stars+"/n"
+#             print(stars)
+
+
+# for i in range(100):
+#         if  i=0 i=i+2:
+#                 print(i, "pair")
+# else:
+#         print(i, "impair")
+
+# 
+for i in range(10):
+    print (i)
+    for j in range(0,10):
+            print(str(i) +"x"+str(j)+"="+str(i*j))
+        
+        
