3613acfb27329bd9270b29f45b4fb726f648e868
Dinara <dinozavrik.dina95@gmail.com>
2019-09-12 16:58:22 +0200

    python

--- test.py
+++ test.py
@@ -26,9 +26,50 @@
 #         print(i, "impair")
 
 # 
-for i in range(10):
-    print (i)
-    for j in range(0,10):
-            print(str(i) +"x"+str(j)+"="+str(i*j))
+#for i in range(10):
+ #   print (i)
+  #  for j in range(0,10):
+   #         print(str(i) +"x"+str(j)+"="+str(i*j))
         
-        
+myInt_list = [10,50,30,45,6,45,855,556,56565,5445,458562,56456,4845] 
+myString_list= ["vie", "belle", "La"]
+myBool_list = [True, False, True, True, True, False, False]
+#print (len (myInt_list))
+#print (len (myString_list))
+#print (len(myBool_list))
+# myInt_list[0]=42
+# print (myInt_list)
+# myString_list[1]="compliqué"
+# print(myString_list)
+# myString_list.append("est")
+# print (myString_list)
+# for i in range(len (myInt_list)):
+#      print (myInt_list[i])   
+# for integer in (myInt_list):
+#         print(integer)
+# for i in range (len (len(myInt_list)-1,-1,-1):
+#     print (i)
+    
+# for i in range (len(myInt_list)):
+#     print(myInt_list[len(myInt_list) -i -1])
+
+# maliste = [10,20,3,4,2,7]
+# myNumber = 6
+# cpt=0
+# for i in range(len(maliste)-1):
+#     if maliste[i]<myNumber:
+#         cpt=cpt+1
+# print(cpt)
+
+    # if maliste[i]<maliste[i+1]:
+    #     print("True")
+    # else: 
+    #     print("False")
+     
+
+
+maliste = [10,20,3,4,2,7]
+n=10
+for i in range(len(maliste)):
+    if n==maliste[i]:
+        print(True,i)
